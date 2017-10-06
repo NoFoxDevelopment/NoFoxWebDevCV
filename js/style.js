f6e3198e864c5f175a562823e30fc396017f2897
NoFoxDevelopment <31908496+NoFoxDevelopment@users.noreply.github.com>
2017-10-05 21:20:03 -0400

    Seriously fixed it this time... Fuck IE.
    Fixed IE detector in style.js for ie.scss.

--- js/style.js
+++ js/style.js
@@ -3,14 +3,45 @@ $(document).ready(function() {
 });
 
 var cssPath = 'style.css';
+var version = detectIE();
 
 if (typeof window.orientation !== 'undefined') {
 	cssPath = 'mobile.css';
 	$('.block dropdown').onclick(function (){
 		$('.dropdown-content').show();
 	});
-} else if (navigator.appName == 'Microsoft Internet Explorer' || 'Netscape') {
+} else if (version != false) {
 	cssPath = 'ie.css';
+	$('#fox').css({strokeDashoffset: 0});
+}
+
+function detectIE() {
+	var ua = window.navigator.userAgent;
+
+	var msie = ua.indexOf('MSIE ');
+	if (msie > 0) {
+	// IE 10 or older => return version number
+	console.log('IE10 or under');
+	return parseInt(ua.substring(msie + 5, ua.indexOf('.', msie)), 10);
+	}
+
+	var trident = ua.indexOf('Trident/');
+	if (trident > 0) {
+	// IE 11 => return version number
+	console.log('IE 11');
+	var rv = ua.indexOf('rv:');
+	return parseInt(ua.substring(rv + 3, ua.indexOf('.', rv)), 10);
+	}
+
+	var edge = ua.indexOf('Edge/');
+	if (edge > 0) {
+	// Edge (IE 12+) => return version number
+	console.log('IE12+');
+	return parseInt(ua.substring(edge + 5, ua.indexOf('.', edge)), 10);
+	}
+
+	// other browser
+	return false;
 }
 
 var fileref = document.createElement('link');
